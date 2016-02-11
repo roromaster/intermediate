5f029a655963d45c8ff5d53c1fad00cdf77dbced
Roro <roromaster@gmail.com>
2016-02-11 22:18:20 +0100

    Creation
    Creation

---
+++ collections/Recipes.js
@@ -0,0 +1,67 @@
+Recipes = new Mongo.Collection('recipes');
+
+Recipes.allow({
+  insert: function(userId, doc){
+    return !!userId;
+  },
+  update: function(userId, doc){
+    return !!userId;
+  },
+  remove: function(userId, doc){
+    return !!userId;
+  }
+});
+
+Ingredient = new SimpleSchema({
+  name: {
+    type: String
+  },
+  amount: {
+    type: String
+  }
+})
+
+
+RecipeSchema = new SimpleSchema({
+  name: {
+    type: String,
+    label: "Name"
+  },
+  desc: {
+    type: String,
+    label: "Description"
+  },
+  ingredients: {
+    type: [Ingredient]
+  },
+  inMenu: {
+    type: Boolean,
+    defaultValue: false,
+    optional: true,
+    autoform: {
+      type: 'hidden'
+    }
+  },
+  author: {
+    type: String,
+    label: "Author",
+    autoValue: function() {
+      return this.userId
+    },
+    autoform: {
+      type: 'hidden'
+    }
+  },
+  createAt: {
+    type: Date,
+    label: "Create At",
+    autoValue: function() {
+      return new Date()
+    },
+    autoform: {
+      type: 'hidden'
+    }
+  }
+});
+
+Recipes.attachSchema(RecipeSchema);
