52a33530353e3411cf8a2b74b5f2985aaf771901
Roro <roromaster@gmail.com>
2016-02-11 22:58:27 +0100

    Adding email validation

--- server/startup.js
+++ server/startup.js
@@ -1,3 +1,10 @@
-Meteor.startup(function () {
-    // code to run on server at startup
+Meteor.startup(function() {
+  // code to run on server at startup
+  process.env.MAIL_URL = 'smtp://smtp.free.fr';
+
+
+  Accounts.config({
+    sendVerificationEmail: true
   });
+
+});
