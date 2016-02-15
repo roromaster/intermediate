5d694ec7d519cbd9b9d600628711e2d6560e29a6
roromaster <roromaster@gmail.com>
2016-02-15 20:16:24 +0100

    Added Analytics Added kadira monitoring

--- server/startup.js
+++ server/startup.js
@@ -1,7 +1,9 @@
 Meteor.startup(function() {
   // code to run on server at startup
-  process.env.MAIL_URL = 'smtp://smtp.free.fr';
+  //process.env.MAIL_URL = 'smtp://smtp.free.fr';
 
+  // Kadira Analytics
+  Kadira.connect('bSZkxCD6aefpSonNN', 'a76e0757-9d57-4427-b0e5-89e69c7ea041');
 
   Accounts.config({
     sendVerificationEmail: true
